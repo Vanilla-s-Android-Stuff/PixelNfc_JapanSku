ce36b848ab6d61d44a5b61e29561f4704a253c55
Vanilla <osu_Vanilla@126.com>
2023-05-28 15:55:47 +0800

    feat[MyModule.kt]: Method hooked, replace to true.

--- app/src/main/java/me/vergedx/pixelnfc_japansku/MyModule.kt
+++ app/src/main/java/me/vergedx/pixelnfc_japansku/MyModule.kt
@@ -1,28 +1,19 @@
 package me.vergedx.pixelnfc_japansku
 
 import de.robv.android.xposed.IXposedHookLoadPackage
-import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
 class MyModule : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
-        XposedBridge.log("Loaded app: " + lpparam.packageName)
-
         XposedHelpers.findAndHookMethod(
             "com.google.android.pixelnfc.provider.DeviceInfoContentProvider",
             lpparam.classLoader, "isDeviceJapanSku", String::class.java,
 
-            object : XC_MethodHook() {
-                override fun beforeHookedMethod(param: MethodHookParam?) {
-                    XposedBridge.log("beforeHookedMethod")
-                    super.beforeHookedMethod(param)
-                }
-
-                override fun afterHookedMethod(param: MethodHookParam?) {
-                    XposedBridge.log("afterHookedMethod")
-                    super.afterHookedMethod(param)
+            object : XC_MethodReplacement() {
+                override fun replaceHookedMethod(param: MethodHookParam?): Any {
+                    return true
                 }
             })
     }
